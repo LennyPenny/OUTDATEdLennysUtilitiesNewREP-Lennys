2a80292b06d7d9cbe472db1602a47e4e1e61c1eb
Lennart Bernhardt <l.bernhardt@live.de>
2013-09-10 22:59:12 +0200

    2.5
    propkill script

---
+++ Lenny's_Scripts/lua/Lenny/pkiller.lua
@@ -0,0 +1,20 @@
+CreateClientConVar("lenny_pkill_speed", 100)
+CreateClientConVar("lenny_pkill_prop", "models/props_c17/furnitureStove001a.mdl")
+CreateClientConVar("lenny_pkill_remover", 0.5)
+
+local function propkill()
+	hook.Add( "CreateMove", "PKill", function(cmd)
+		cmd:SetMouseWheel(GetConVarNumber("lenny_pkill_speed"))
+	end)
+	RunConsoleCommand("gm_spawn", GetConVarString("lenny_pkill_prop"))
+	RunConsoleCommand("+attack")
+	timer.Simple(.01, function()
+		RunConsoleCommand("-attack")
+	end)
+	timer.Simple(GetConVarNumber("lenny_pkill_remover"), function()
+		hook.Remove("CreateMove", "PKill")
+		RunConsoleCommand("undo")
+	end )
+end
+
+concommand.Add("lenny_pkill", propkill)
