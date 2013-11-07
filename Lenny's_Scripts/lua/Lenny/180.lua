875945b3cb9cf578b3058e3e2e40324dcb1e0339
LennyPenny <l.bernhardt@live.de>
2013-11-07 17:13:27 +0100

    Update 180.lua

--- Lenny's_Scripts/lua/Lenny/180.lua
+++ Lenny's_Scripts/lua/Lenny/180.lua
@@ -1,11 +1,6 @@
 function doa180()
-	aim1 = ply:GetAimVector():Angle()
-	print(aim1)
+	aim = ply:GetAimVector():Angle()
 	ply:SetEyeAngles(aim1 - Angle(0, 180, 0))
-	//local ang = (vec1 - vec2):Angle()
-	print(vec1)
-
-	//ply:SetEyeAngles(ang)
 end
 
 concommand.Add("lenny_180", doa180)
